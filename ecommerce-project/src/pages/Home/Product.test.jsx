c156ced68e95974900323b53a5e089c9e92de995
beatriz.cubells <beatriz.cubells@bbraun.com>
2026-01-26 19:30:08 +0100

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/Home/Product.test.jsx
+++ ecommerce-project/src/pages/Home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios'; // This is already a mocked version of axios because of the vi.mock call
@@ -7,8 +7,12 @@ import { Product } from './Product';
 vi.mock('axios'); // Mock an entire npm package
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+    let product;
+
+    let loadCart;
+
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,7 +24,10 @@ describe('Product component', () => {
             keywords: ["socks", "sports", "apparel"]
         };
 
-        const loadCart = vi.fn(); // Mock function that does nothing
+        loadCart = vi.fn(); // Mock function that does nothing
+    });
+
+    it('displays the product details correctly', () => {
         render(<Product product={product} loadCart={loadCart} />);
         expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
         expect(screen.getByText('$10.90')).toBeInTheDocument();
@@ -30,19 +37,6 @@ describe('Product component', () => {
     });
 
     it('adds a product to the cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-
-        const loadCart = vi.fn(); // Mock function that does nothing
         render(<Product product={product} loadCart={loadCart} />);
         const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
