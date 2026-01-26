738fc972c3ca2bb0b64d72cfdbfcd293f3e10d50
beatriz.cubells <beatriz.cubells@bbraun.com>
2026-01-26 10:27:02 +0100

    Checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
+import CheckoutPage from './pages/CheckoutPage.jsx';
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} /> {/* path="/" is the same as index */}
-      <Route path="checkout" element={<div><h1>Checkout Page</h1></div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
