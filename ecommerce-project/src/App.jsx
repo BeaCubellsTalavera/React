b14b60ecbf725e0d09dbd347a209300f1260b722
beatriz.cubells <beatriz.cubells@bbraun.com>
2026-01-26 10:19:51 +0100

    Add routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,13 @@
+import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Routes>
+      <Route index element={<HomePage />} /> {/* path="/" is the same as index */}
+      <Route path="checkout" element={<div><h1>Checkout Page</h1></div>} />
+    </Routes>
   )
 }
 
